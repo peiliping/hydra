561211317eb129277843e0afe711f99fa9ed8b71
peiliping <peilipingplp@gmail.com>
2020-04-26 19:15:05 +0800

    u

--- start/startBiki.sh
+++
@@ -1,1 +0,0 @@
-java -classpath hydra-1.0-SNAPSHOT.jar com.github.hydra.client.Start --logLevel debug -ssl --host wspool.mpuuss.top --port 443 --path /kline-api/ws --subscribe '{"event":"sub","params":{"channel":"market_bikiusdt_kline_30min","cb_id":"bikiusdt"}}' --heartBeat '{"pong":%s}' --ungzip
