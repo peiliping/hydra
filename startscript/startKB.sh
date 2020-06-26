591d38909ce04ec32204692d28781559530889fd
peiliping <peilipingplp@gmail.com>
2020-06-27 02:04:41 +0800

    add script

---
+++ startscript/startKB.sh
@@ -0,0 +1,1 @@
+java -classpath ../target/hydra-1.0-SNAPSHOT.jar com.github.hydra.client.Start --logLevel debug -ssl --host www.kbtrade.me --port 9443 --path /ws  --subscribeInterval 20  --heartBeat '{"event":"ping"}' --unGzipJson --subscribe '{"event":"sub","biz":"exchange","type":"ticker","pairCode":"ALL"}'
